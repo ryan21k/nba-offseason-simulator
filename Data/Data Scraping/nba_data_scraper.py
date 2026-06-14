267f0ba251b18bef995cd5f9a290698f099fe2b5
Ryan Kappil <ryangkappil@gmail.com>
2026-06-14 04:54:41 -0500

    implemented nba data ingestion pipeline with csv export

--- Data/Data Scraping/nba_data_scraper.py
+++ Data/Data Scraping/nba_data_scraper.py
@@ -1,20 +1,25 @@
-from bs4 import BeautifulSoup
+from nba_api.stats.static import teams
+from nba_api.stats.endpoints import leaguedashteamstats
 import pandas as pd
-import requests
 import time
 import os
-import logging
 
-OUTPUT_DIRECTORY = "Data/Standings" #path that stores the csv files
+season_data = [] #empty list that will store all of the NBA season data from 2014-15 season to 2025-26 season
 
-HEADERS = {
-    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36"
-}
-
-nba_team_data = [] #list that will store all 30 nba teams data
-
-for year in range(2015, 2026):
-    url = f"https://www.basketball-reference.com/leagues/NBA_{year}.html"
-    print(f"Scraping the NBA {year - 1}-{str(year)[-2:]} season...")
+for year in range(2015, 2027):
+    print(f"Collecting data from NBA {year - 1}-{str(year)[-2:]} Season  ...")
     
+    # obtain data frame for current season and add it to season_data list
+    df = leaguedashteamstats.LeagueDashTeamStats(season = f"{year-1}-{str(year)[-2:]}").get_data_frames()[0]
+    df["SEASON"] = f"{year-1}-{str(year)[-2:]}"
+    season_data.append(df)
     
+    time.sleep(1)
+
+os.makedirs("../Raw Data", exist_ok = True) #checks to see if the folder exists
+
+#adds the items within season_data into csv file stored within Raw Data folder
+nba_hist_stats = pd.concat(season_data)
+nba_hist_stats.to_csv("../Raw Data/historical_team_data.csv", index = False)
+
+print("Finished adding data content to historical_team_data.csv")
